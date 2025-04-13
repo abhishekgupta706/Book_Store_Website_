fc10e89674115a99f15983a4bd878f071051906c
ABHISHEK <guptaabhishek9717@gmail.com>
2025-04-13 07:12:36 +0530

    adding_build

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     }; 
     console.log(data);
-    await axios.post("http://localhost:4001/api/users/login", userInfo)
+    await axios.post("https://book-ih42.onrender.com/api/users/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
