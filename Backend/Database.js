a662ed72a32334fd9a27eef317bc313da4d9db4e
ABHISHEK <guptaabhishek9717@gmail.com>
2025-04-18 00:31:21 +0530

    Updated

--- Backend/Database.js
+++ Backend/Database.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const  connectDB = async () => {
-  const URI = process.env.MONGO_URL;
+  const URL = process.env.MONGO_URL;
    try {
-    await mongoose.connect(URI);
+    await mongoose.connect(URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
